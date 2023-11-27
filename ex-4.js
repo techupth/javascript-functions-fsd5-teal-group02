be73821ac33bca99accdc4fde593a4b30244a6fe
Palm <palml1lax@gmail.com>
2023-11-27 22:40:48 +0700

    feat: finish exercises.

--- ex-4.js
+++ ex-4.js
@@ -1,2 +1,30 @@
 // Start coding here
 let calculator = {};
+
+calculator.add = (a, b) => {
+  return a + b;
+};
+
+calculator.subtract = (a, b) => {
+  return a - b;
+};
+
+calculator.multiply = (a, b) => {
+  return a * b;
+};
+
+calculator.devide = (a, b) => {
+  return a / b;
+};
+
+let addResult;
+
+let divideResult;
+
+addResult = calculator.add(10, 20);
+
+console.log(addResult);
+
+divideResult = calculator.devide(3000, 10);
+
+console.log(divideResult);
